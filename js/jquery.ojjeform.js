4c60013c96d13c5be06d2d6d5b8c56ac0943180c
Olof <olofresare@gmail.com>
2015-02-04 13:56:34 +0100

    Jqueryfied all variables so we know if its an object so we know if we can run jquery methods on them.

--- js/jquery.ojjeform.js
+++ js/jquery.ojjeform.js
@@ -1,8 +1,9 @@
 (function($) {
+  "use strict";
   
-  $.ojjeform = function(forms, options) {  
+  $.ojjeform = function($forms, options) {  
     
-    forms = forms.filter(':not(".ojjeform")');
+    $forms = $forms.filter(':not(".ojjeform")');
           
     /* Set our default settings */
     var defaults = {
@@ -16,159 +17,195 @@
     /* Merge the defaults with the user provided options recursive */
     var settings = $.extend(true, {}, defaults, options);    
 
-    forms.each(function(e, v) {
-      var form = $(v);
-      var formItems = form.find('input[type=checkbox], input[type=radio], select');
-      form.addClass('ojjeform');
+    $forms.each(function(e, v) {
+      var $form = $(v);
+      var $formItems = $form.find('input[type=checkbox], input[type=radio], select');
+      $form.addClass('ojjeform');
 
       if (settings.types.checkbox == false) {
-        formItems = formItems.not('[type=checkbox]');
+        $formItems = $formItems.not('[type=checkbox]');
       }
       if (settings.types.radio == false) {
-        formItems = formItems.not('[type=radio]');
+        $formItems = $formItems.not('[type=radio]');
       }
       if (settings.types.select == false) {
-        formItems = formItems.not('select');
+        $formItems = $formItems.not('select');
       }
   
-      formItems.each(function(i, o) {
-        var item = $(o);
+      $formItems.each(function(i, o) {
+        var $item = $(o);
         var tagType = o.tagName.toLowerCase();
         
         if (tagType == 'select') {
-          item.addClass('form-select');
-          item.wrap('<div class="form-type-select"></div>');
-          var parent = item.parent();
-          itemType = 'ojjeform-select';
-          var name = item.prop('name');
-          var label = form.find('label[for="' + name + '"]');
-          label.prependTo(parent);
+          $item.addClass('form-select');
+          $item.wrap('<div class="form-type-select"></div>');
+          var $parent = $item.parent();
+          var name = $item.prop('name');
+          var $label = $form.find('label[for="' + name + '"]');
+          $label.prependTo($parent);
+          var itemType = 'ojjeform-select';
           var selectLink = '<div class="ojjeform-select-chosen"><a href="#" class="ojjeform-select-chosen-link"></a></div>';
-          parent.append(selectLink);
+          $parent.append(selectLink);
           var listClass = itemType + '-list';
-          var selectList = $('<ul class="' + listClass + '"/>');
-          parent.append(selectList);
+          var $selectList = $('<ul class="' + listClass + '"/>');
+          $parent.append($selectList);
     
           var optionsArray = [];                
-          var listOptions = item.find('option');
+          var $listOptions = $item.find('option');
     
-          listOptions.each(function(k, opt) {
-            var optObj = $(opt);
-            optionsArray.push('<li><a class="' + itemType + '" href="#" data-value="' + optObj.val() + '">' + optObj.text() + '</a></li>');
+          $listOptions.each(function(k, opt) {
+            var $optObj = $(opt);
+            optionsArray.push('<li><a class="' + itemType + '" href="#" data-value="' + $optObj.val() + '">' + $optObj.text() + '</a></li>');
           });   
     
           var optionString = optionsArray.join('');
-          selectList.append(optionString);
+          $selectList.append(optionString);
     
-          var selectedValue = item.find('option:selected').text();
-          var selectedWrapper = parent.find('.ojjeform-select-chosen');
-          var selectedLi = selectedWrapper.find('.ojjeform-select-chosen-link');
+          var selectedValue = $item.find('option:selected').text();
+          var $selectedWrapper = $parent.find('.ojjeform-select-chosen');
+          var $selectedLi = $selectedWrapper.find('.ojjeform-select-chosen-link');
           if (selectedValue.length > 0) {
-            selectedLi.text(selectedValue);
+            $selectedLi.text(selectedValue);
           }        
     
-          selectList.slideUp(0, function() {
-            selectList.removeClass('open');
+          $selectList.slideUp(0, function() {
+            $selectList.removeClass('open');
           });
         } else if (tagType == 'input') {
           var itemType = $(o).prop('type');
-          item.addClass('form-' + itemType); 
-          item.wrap('<div class="form-type-' + itemType + '"></div>');
-          var parent = item.parent();    
-          var name = item.prop('name');
-          var label = form.find('label[for="' + name + '"]');
-          label.prependTo(parent);          
-          var link = '<a href="#" class="ojjeform-' + itemType + '"><span class="icon-marker"></span><span class="icon"></span><span class="text">' + label.text() + '</span></a>';
-          parent.append(link);
+          $item.addClass('form-' + itemType); 
+          $item.wrap('<div class="form-type-' + itemType + '"></div>');
+          var $parent = $item.parent();    
+          
+          var id = $item.prop('id');
+          if (typeof id !== typeof undefined && id !== false && id.length > 0) {
+             var $label = $form.find('label[for="' + id + '"]');
+          } else {
+            var name = $item.prop('name');
+            var $label = $form.find('label[for="' + name + '"]');
+          }
+
+          $label.prependTo($parent);          
+          var link = '<a href="#" class="ojjeform-' + itemType + '"><span class="icon-marker"></span><span class="icon"></span><span class="text">' + $label.text() + '</span></a>';
+          $parent.append(link);
         }
       });
     });
     
-    forms.on('click', 'a.ojjeform-checkbox', function() {
-      var link = $(this);
-      var parent = link.parent();
-      var checkbox = parent.find('input.form-checkbox');
-      if (checkbox.is(':disabled') == false) {            
-        if (parent.hasClass('active') == false) {
-          parent.addClass('active');
-          checkbox.prop("checked", true).trigger('change');
+    $forms.on('click', 'a.ojjeform-checkbox', function() {
+      var $link = $(this);
+      var $parent = $link.parent();
+      var $checkbox = parent.find('input.form-checkbox');
+      if ($checkbox.is(':disabled') == false) {
+        if ($parent.hasClass('active') == false) {
+          $parent.addClass('active');
+          $checkbox.prop("checked", true).trigger('change');
         } else {
-          parent.removeClass('active');
-          checkbox.prop("checked", false).trigger('change');
+          $parent.removeClass('active');
+          $checkbox.prop("checked", false).trigger('change');
         }
       }
       return false;
     });
-    
-    forms.on('click', 'a.ojjeform-radio', function() {
-      var link = $(this);
-      var parent = link.parent();
-      var radio = parent.find('input.form-radio');
-      if (radio.is(':disabled') == false) {    
-        if (parent.hasClass('active') == false) {
-          var radios = link.parents('.form-radios');
-          radios.find('.form-type-radio').removeClass('active');
-          radios.find("input:radio").prop("checked", false);
-          parent.addClass('active');   
-          parent.find('input.form-radio').prop("checked", true).trigger("change");    
-        } else {
-          parent.find('input.form-radio').prop("checked", false);
-          parent.removeClass('active');
-        }
+
+    $forms.on('click', '.form-radios label', function(e) {
+      e.preventDefault();
+      var $label = $(this);
+      var target = $label.prop('for');
+      var $parent = $label.closest('.form-type-radio');
+      var $target = $parent.find('input[id="' + target + '"]');
+      if ($target.length == 0) {
+        $target = $parent.find('input[name="' + target + '"]');
       }
-      return false;
+
+      if ($target.is(':checked')) {
+        $target.prop('checked', false);
+        $target.trigger('change');
+      } else {
+        $target.prop('checked', true);
+        $target.trigger('change');
+      }
     });
-    
-    forms.on('click', 'a.ojjeform-select', function() {
-      var link = $(this);
-      var parentLi = link.parent();
-      var parentUl = parentLi.parent();
-      var wrapper = parentUl.parent();
-      var select = wrapper.find('select');
-      var selectedWrapper = wrapper.find('.ojjeform-select-chosen');
-      var selectedLink = selectedWrapper.find('.ojjeform-select-chosen-link');
-      if (select.is(':disabled') == false) {
-        if (link.hasClass('open') == false) {
-          var selectList = link.parents('ul');
-          selectList.find('li').each(function(index, selectObj) {
+
+    $forms.on('change', '.form-radios input[type="radio"]', function() {
+      var $radio = $(this);
+      var $grandParent = $radio.closest('.form-radios');
+
+      $.each($grandParent.find('input[type="radio"]'), function(k, v) {
+        var $v = $(v);
+        var $parent = $v.closest('.form-type-radio');
+
+        if ($v.is(':checked')) {
+          $parent.addClass('active');
+        } else {
+          $parent.removeClass('active');
+        }
+      });
+    });
+
+    $forms.on('click', 'a.ojjeform-select', function() {
+      var $link = $(this);
+      var $parentLi = $link.parent();
+      var $parentUl = $parentLi.parent();
+      var $wrapper = $parentUl.parent();
+      var $select = $wrapper.find('select');
+      var $selectedWrapper = $wrapper.find('.ojjeform-select-chosen');
+      var $selectedLink = $selectedWrapper.find('.ojjeform-select-chosen-link');
+      if ($select.is(':disabled') == false) {
+        if ($link.hasClass('open') == false) {
+          var $selectList = $link.parents('ul');
+          $selectList.find('li').each(function(index, selectObj) {
             $(selectObj).removeClass('active');
           });
-          parentLi.addClass('active');   
-          var chosenVal = link.data('value');
-          selectedLink.text(link.text());    
-          selectList.slideUp(200, function() {
-            selectList.removeClass('open');
-            var optionToChoose = select.find('option[value="'+chosenVal+'"]');
-            optionToChoose.prop('selected', true).trigger('change');
+          $parentLi.addClass('active');
+          var chosenVal = $link.data('value');
+          $selectedLink.text($link.text());
+          $selectList.slideUp(200, function() {
+            $selectList.removeClass('open');
+            var o$ptionToChoose = $select.find('option[value="' + chosenVal + '"]');
+            $optionToChoose.prop('selected', true).trigger('change');
           });
         }
       }
       return false;
     });
-    
-    forms.on('click', 'a.ojjeform-select-chosen-link', function() {
-      var link = $(this);
-      var parent = link.parent();
-      var wrapper = parent.parent();
-      var select = wrapper.find('select');
-      if (select.is(':disabled') == false) {
-        var selectList = wrapper.find('ul.ojjeform-select-list');
-        if (selectList.hasClass('open')) {
-          selectList.slideUp(200, function(){
-            selectList.removeClass('open');
+
+    $forms.on('click', 'a.ojjeform-select-chosen-link', function() {
+      var $link = $(this);
+      var $parent = $link.parent();
+      var $wrapper = $parent.parent();
+      var $select = $wrapper.find('select');
+      var $form = $link.closest('form');
+      if ($select.is(':disabled') == false) {
+        $.each($form.find('ul.ojjeform-select-list.open'), function(k, v) {
+          var $list = $(v);
+          var $listParent = $(v).closest('.form-type-select');
+          var $listLink = $listParent.find('.ojjeform-select-chosen-link');
+          $list.slideUp(200, function() {
+            $list.removeClass('open');
+            $listLink.removeClass('open');
+          });
+        });
+
+        var $selectList = $wrapper.find('ul.ojjeform-select-list');
+        if ($selectList.hasClass('open')) {
+          $selectList.slideUp(200, function(){
+            $selectList.removeClass('open');
+            $link.removeClass('open');
           });
         } else {
-          selectList.slideDown(200, function(){
-            selectList.addClass('open');
+          $selectList.slideDown(200, function(){
+            $selectList.addClass('open');
+            $link.addClass('open');
           });
         }
       }
       return false;
     });
-    
-    forms.on('reset', function() {
-      var form = $(this);
-      form.find('.active').removeClass('active');
+
+    $forms.on('reset', function() {
+      var $form = $(this);
+      $form.find('.active').removeClass('active');
     });
     
   };
